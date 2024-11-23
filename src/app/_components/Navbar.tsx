919667a85230c3ed1ff842b65b08cc4798301579
Jose <josemaurette1234@gmail.com>
2024-11-23 15:59:10 -0500

    Created Navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Search } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Mountain } from "lucide-react";
+import { Camera } from "lucide-react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="fixed top-5 w-full flex justify-center items-center gap-12 shadow-md p-4 z-10">
+      <Search />
+      <Mountain />
+      <Camera />
+      {/* Todo: Add href for each icon */}
+    </div>
+  );
 };
 
 export default Navbar;
