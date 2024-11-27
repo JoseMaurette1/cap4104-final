be9ca22e2ef920e8d00a101706bf1bb208e8623b
Jose <josemaurette1234@gmail.com>
2024-11-26 20:25:23 -0500

    Mobile Support

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <>
-      <div className="absolute top-5 left-5 z-10">
+      <div className="absolute top-4 left-4 sm:left-5 z-10">
         <Link href={"/"}>
-          <h1 className="font-bold text-xl border-double border-gray-400 border-4 p-2 rounded hover:border-solid transform transition-transform duration-300 hover:translate-y-[-4px]">
+          <h1 className="font-bold text-xl sm:text-xl border-double border-gray-400 border-4 p-2 rounded hover:border-solid transform transition-transform duration-300 hover:translate-y-[-4px]">
             TrailTales
           </h1>
         </Link>
       </div>
-      <div className="absolute p-4 z-10 top-5 right-5">
+      <div className="absolute top-4 right-4 sm:right-5 p-2 z-10">
         <ModeToggle />
       </div>
     </>
