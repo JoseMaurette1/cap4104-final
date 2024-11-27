33b5e55df55064387cb87cc2c07e457258facc2e
Jose <josemaurette1234@gmail.com>
2024-11-26 19:53:27 -0500

    Made Signup wider

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import SignUp from "../_components/SignUp";
 
 const page = () => {
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
+  return <SignUp />;
 };
 
 export default page;
