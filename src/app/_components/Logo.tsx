ee9f95f336bf455f984c06aeea37bafad436bec8
Jose <josemaurette1234@gmail.com>
2024-11-29 16:42:30 -0500

    Logo doesnt redirect to Home if not signed in

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Logo = () => {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
+  const isSignInPage = pathname === "/Signup";
+  const noAccessLogo = isHomePage || isSignInPage;
+
   return (
     <>
       <div className="absolute top-4 left-4 sm:left-5 z-10">
-        <Link href={"/"}>
+        {!noAccessLogo ? (
+          <Link href={"/Home"}>
+            <h1 className="font-bold text-xl sm:text-xl border-double border-gray-400 border-4 p-2 rounded hover:border-solid transform transition-transform duration-300 hover:translate-y-[-4px]">
+              TrailTales
+            </h1>
+          </Link>
+        ) : (
           <h1 className="font-bold text-xl sm:text-xl border-double border-gray-400 border-4 p-2 rounded hover:border-solid transform transition-transform duration-300 hover:translate-y-[-4px]">
             TrailTales
           </h1>
-        </Link>
+        )}
       </div>
       <div className="absolute top-4 right-4 sm:right-5 p-2 z-10">
         <ModeToggle />
