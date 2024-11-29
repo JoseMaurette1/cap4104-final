55daf4d35b4372eeab569228d63a5360ce1616fe
Jose <josemaurette1234@gmail.com>
2024-11-29 18:45:17 -0500

    Made Searchbar

---
+++ src/app/_components/Searchbar.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import {
+  Search,
+  Lightbulb,
+  Mountain,
+  Settings,
+  User,
+  House,
+} from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+const Searchbar = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedCommand, setSelectedCommand] = useState<string | null>(null);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <p className="text-md text-muted-foreground">
+        Press{" "}
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[20px] font-medium text-muted-foreground opacity-100">
+          <span className="text-md">⌘</span>J
+        </kbd>
+      </p>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <Link href={"/Search"}>
+              <CommandItem>
+                <Search className="mr-2" />
+                <span>Search</span>
+              </CommandItem>
+            </Link>{" "}
+            <Link href={"/Trails"}>
+              <CommandItem>
+                <Mountain />
+                <span>Choose Trail</span>
+              </CommandItem>
+            </Link>
+            <Link href={"https://cap4104.vercel.app"}>
+              <CommandItem>
+                <Lightbulb />
+                <span>Prototype Website</span>
+              </CommandItem>
+            </Link>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <Link href={"/Profile"}>
+              <CommandItem>
+                <User />
+                <span>Profile</span>
+                <CommandShortcut>⌘P</CommandShortcut>
+              </CommandItem>
+            </Link>
+            <Link href={"/Home"}>
+              <CommandItem>
+                <House />
+                <span>Home</span>
+                <CommandShortcut>⌘B</CommandShortcut>
+              </CommandItem>
+            </Link>
+            <Link href={"/Settings"}>
+              <CommandItem>
+                <Settings />
+                <span>Settings</span>
+                <CommandShortcut>⌘S</CommandShortcut>
+              </CommandItem>
+            </Link>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+export default Searchbar;
