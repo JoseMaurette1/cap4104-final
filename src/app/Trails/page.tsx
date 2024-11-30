2daa4f9a09c0f1290a0824bd0197f88f9534bbb1
Jose <josemaurette1234@gmail.com>
2024-11-29 19:39:36 -0500

    Made Trails Page

---
+++ src/app/Trails/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Searchbar from "../_components/Searchbar";
+
+const page = () => {
+  return (
+    <>
+      <Searchbar />
+    </>
+  );
+};
+
+export default page;
