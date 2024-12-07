66d5421fce80cb82acf660a500b4eb3934dc6a3c
Jose <josemaurette1234@gmail.com>
2024-12-07 14:02:25 -0500

    Made Data Evaluation Tab

---
+++ src/app/DesignEvaluation/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <iframe src="/Data.pdf" width={800} height={800}></iframe>
+    </div>
+  );
+};
+
+export default page;
