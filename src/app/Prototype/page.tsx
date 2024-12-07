85b2cc40a6ed1c8cd26de5a9f0b4570d6b773a74
Jose <josemaurette1234@gmail.com>
2024-12-07 16:30:36 -0500

    Gave instructions to scroll

--- src/app/Prototype/page.tsx
+++ src/app/Prototype/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "next/image";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import StyledText from "../_components/StyledText";
 
 const page = () => {
   return (
-    <div>
+    <div className="text-center">
+      <StyledText>Prototype</StyledText>
       <ScrollArea className="h-96 max-w-5xl rounded-md border">
         <Image src={"/Prototype.png"} height={1200} width={800} alt="pic" />
       </ScrollArea>
