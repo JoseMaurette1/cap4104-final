ff6af0c2016f080c31c1a637212ed9603c1d444c
Jose <josemaurette1234@gmail.com>
2025-02-11 10:10:57 -0500

    Added Command

--- src/app/Trails/page.tsx
+++ src/app/Trails/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1>CTRL + J to get started</h1>
+      <h1 className="text-3xl">⌘ + J to get started.</h1>
+      <p>Use the search bar to navigate universally.</p>
     </div>
   );
 };
